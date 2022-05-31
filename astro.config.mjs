0477d0330afee3af2245ed7b30e9f22842a4b425
Maysen Greenwood <greenwood.maysen@gmail.com>
2022-06-01 00:00:03 +1200

    remove preact integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+	//integrations: [preact()],
 	site: 'https://maysentg.github.io/',
 	base: 'astro-personal-portfolio'
 });
