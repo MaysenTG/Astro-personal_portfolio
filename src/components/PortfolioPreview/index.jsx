eebb4cc053570a1235c508de9c373834a806f243
Maysen Greenwood <greenwood.maysen@gmail.com>
2022-06-05 18:20:56 +1200

    Implement GraphCMS data into website

--- src/components/PortfolioPreview/index.jsx
+++ src/components/PortfolioPreview/index.jsx
@@ -1,25 +1,22 @@
-import { h } from 'preact';
+
 import Styles from './styles.module.scss';
 
 function PortfolioPreview({ project }) {
-	const { frontmatter } = project;
+	const frontmatter = project;
 	return (
-		<div className={Styles.card}>
-			<div className={Styles.titleCard} style={`background-image:url(${frontmatter.img})`}>
-				<h1 className={Styles.title}>{frontmatter.title}</h1>
+		<div className={Styles.card} style={`background-image:url(${frontmatter.projectImage.url}); background-size: contain;`}>
+			<div className={Styles.titleCard}>
+				<h1 className={Styles.title}></h1>
 			</div>
 			<div className="pa3">
-				<p className={`${Styles.desc} mt0 mb2`}>{frontmatter.description}</p>
-				<div className={Styles.tags}>
-					Tagged:
-					{frontmatter.tags.map((t) => (
-						<div className={Styles.tag} data-tag={t}>
-							{t}
-						</div>
-					))}
-				</div>
 				<a className={Styles.link} href={project.url}>
-					<span className={Styles.linkInner}>View</span>
+					<p style="font-size: 2.5vw;" className="ml2 mr2">{frontmatter.title}</p>
+					<p style="font-size: 1vw;" className="ml2 mr2">{frontmatter.skills}</p>
+					<p style="font-size: 1vw;" className={`${Styles.desc} ml2 mr2 mt0 mb2`}>{frontmatter.description}</p>
+					<div className="ml2" style="display: flex; flex-direction: row;">
+						<span className={Styles.linkInner}>Github</span>&nbsp;
+						<span className={Styles.linkInner}>Live preview</span>
+					</div>
 				</a>
 			</div>
 		</div>
