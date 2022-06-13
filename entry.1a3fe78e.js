1c73f7f4f298663df10159af0de01af2ad26bf9b
MaysenTG <MaysenTG@users.noreply.github.com>
2022-06-13 03:39:26 +0000

    deploy: d5a5bb010d57e10e07f9f941fad9a83ae64fa319

--- entry.1a3fe78e.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",e=>{try{document.querySelector("a.active").classList.remove("active")}catch{}document.getElementById(window.location.pathname).classList.toggle("active")});
